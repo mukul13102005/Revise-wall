044caa4e0ca3eba86f8d4c67e12d478b4192f912
Mukul Kumar <119054799+mukul13102005@users.noreply.github.com>
2026-04-16 23:05:55 +0530

    Initialize main.jsx with React and ReviseWall component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReviseWall from "./ReviseWall.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ReviseWall />
+  </React.StrictMode>
+);
